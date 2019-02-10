b757a0708051b6b3a2ee8580c0427ecee91db86a
Dennis Verstappen <dennisverstappen1@gmail.com>
2019-02-10 17:00:12 +0100

    log2

--- websitemp/settings.py
+++ websitemp/settings.py
@@ -28,7 +28,7 @@ with open('secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "monkeyparliament.herokuapp.com", ".herokuapp.com"]
 
 
 # Application definition
